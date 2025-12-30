29bc5965d7c140231f5dd55c0b4274389f480ed9
Emulie Chhor <emulie.chhor@hotmail.com>
2025-12-30 12:15:23 -0500

    removed unused pure pursuit configs

--- packages/pure_pursuit_control/include/pure_pursuit_controller/pure_pursuit_controller.py
+++ packages/pure_pursuit_control/include/pure_pursuit_controller/pure_pursuit_controller.py
@@ -16,13 +16,17 @@ class PurePursuitController:
         self.last_found_index = 0
         self.current_pos = [0.0, 0.0]
         self.current_heading = 0.0
-        #  self.num_frames = 400  # FIXME needed?
 
     def update_parameters(self, parameters):
         self.parameters = parameters
 
     def compute_control_action(self, path_points):
         lookahead_distance = self.parameters["~lookahead_distance"].value
+        width = self.parameters["~width"].value
+        omega_factor = self.parameters["~omega_factor"].value
+        v_bar = self.parameters["~v_bar"].value
+        v_bar_min = self.parameters["~v_bar_min"].value
+        v_bar_max = self.parameters["~v_bar_max"].value
 
         # 1. Find goal points
         goal_point, _ = find_goal_point(
@@ -45,14 +49,8 @@ class PurePursuitController:
         # distance
         L_d = math.sqrt(dx**2 + dy**2)
 
-        width = self.parameters["~width"].value
-        omega_factor = self.parameters["~omega_factor"].value
-        v_bar = self.parameters["~v_bar"].value
-        v_bar_min = self.parameters["~v_bar_min"].value
-        v_bar_max = self.parameters["~v_bar_max"].value
-
         R = np.abs(L_d / (2.0 * np.sin(turn_error)))
         v = np.clip(v_bar * R, v_bar_min, v_bar_max)
-
         omega = omega_factor * (width * np.sin(turn_error) * v) / L_d
+
         return v, omega
