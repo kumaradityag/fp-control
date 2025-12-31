96b9f2eb944decab9ed043c19c8e06f2f9758152
Emulie Chhor <emulie.chhor@hotmail.com>
2025-12-31 14:05:33 -0500

    added launch scripts for following carrot

---
+++ launchers/lane-following-carrot-virtual-inner.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source /environment.sh
+
+# initialize launch file
+dt-launchfile-init
+
+# YOUR CODE BELOW THIS LINE
+# ----------------------------------------------------------------------------
+
+# launching app
+dt-exec roslaunch --wait duckietown_demos lane_following_carrot.launch pp_param_file_name:=virtual_inner traj_param_file:=inner
+
+
+# ----------------------------------------------------------------------------
+# YOUR CODE ABOVE THIS LINE
+
+# wait for app to end
+dt-launchfile-join
